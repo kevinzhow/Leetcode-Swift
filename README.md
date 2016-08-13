234c8051fb3aff32347a71dffe6894b627d2c830
Kevin <kevinchou.c@gmail.com>
2016-08-14 01:27:32 +0800

    Improve README

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Leetcode-Swift
 
 Leetcode Practice For Fun
+
+| Lesson  | Work | Demo |
+| ------------- | ------------- |
+| [#1 Two Sum](https://leetcode.com/problems/two-sum/)  | [Swift](https://github.com/kevinzhow/Leetcode-Swift/blob/master/TwoSum.swift)  | [IBM Sandbox](http://swiftlang.ng.bluemix.net/#/repl/57af57203451d5f71ad95d38) |
--- TwoSum.swift
+++ TwoSum.swift
@@ -1,14 +1,15 @@
 // Source : https://leetcode.com/problems/two-sum/
+// Demo   ：http://swiftlang.ng.bluemix.net/#/repl/57af57203451d5f71ad95d38
 // Author : Kevin Zhow
 // Date   : 2016-08-14
 /****
  Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- 
+
  You may assume that each input would have exactly one solution.
- 
+
  Example:
  Given nums = [2, 7, 11, 15], target = 9,
- 
+
  Because nums[0] + nums[1] = 2 + 7 = 9,
  return [0, 1].
  ****/
@@ -20,33 +21,29 @@ class Solution {
             for (tempIndex, tempNum) in nums.enumerated() {
                 if num + tempNum == target && index != tempIndex {
                     return [index, tempIndex]
-                } 
+                }
             }
         }
         return []
     }
-    
+
     // Use hash to boost search performance
     func twoSumAdvanced(nums: [Int], target: Int) -> [Int] {
         var answerNums: [Int:Int] = [:]
-        var result: [Int] = []
-        
+
         for (index, num) in nums.enumerated() {
             if let answer = answerNums[num] {
                 return [answer, index]
             } else {
-                // Store completeNum with current index to have an faster ending when we meet completeNum 
+                // Store completeNum with current index to have an faster ending when we meet completeNum
                 let completeNum = target - nums[index]
                 answerNums[completeNum] = index
             }
         }
-        
-        return result
+        return []
     }
 }
 
 let solution = Solution()
 let result = solution.twoSum(nums: [3, 2, 4], target: 6)
 let resultAdvanced = solution.twoSumAdvanced(nums: [3, 2, 4], target: 6)
-
-
